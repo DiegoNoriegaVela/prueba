9ffd3cd1107460269722d3ea245142bd6a109ae9
Diego Noriega Vela <88636928+DiegoNoriegaVela@users.noreply.github.com>
2025-09-03 15:38:17 -0500

    Update Application.java

--- gss-crypto-tool/src/main/java/com/scotiabank/gssvault/Application.java
+++ gss-crypto-tool/src/main/java/com/scotiabank/gssvault/Application.java
@@ -2,237 +2,178 @@ package com.scotiabank.gssvault;
 
 import com.scotiabank.gss.crypto.AESCBCCipher;
 import com.scotiabank.gss.crypto.AESECBCipher;
+import com.scotiabank.gss.crypto.AESGCMCipher;
 import com.scotiabank.gssvault.model.EnumAESMode;
 import com.scotiabank.gssvault.model.EnumAESType;
 import com.scotiabank.gssvault.model.EnumInputType;
-import com.scotiabank.gss.crypto.AESGCMCipher;
 import com.scotiabank.gssvault.util.AESFileCipher;
 
 import java.security.GeneralSecurityException;
 
 import static com.scotiabank.gssvault.model.EnumAESType.*;
-import static com.scotiabank.gssvault.model.EnumInputType.FILE;
-import static com.scotiabank.gssvault.util.Constants.*;
 import static com.scotiabank.gssvault.model.EnumAESMode.*;
-import static com.scotiabank.gssvault.model.EnumInputType.INVALID_INPUT_TYPE;
+import static com.scotiabank.gssvault.model.EnumInputType.*;
+import static com.scotiabank.gssvault.util.Constants.*;
 
 public class Application {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
+        String type = null;
+        String mode = null;
+        String text = null;
+        String textInputType = null;
+        String key = null;
+        String iv = null;
+        String outPutFile = null;
 
-//		boolean demo_sha256 = true;
-//
-//		if (demo_sha256){
-//			String result = Hashing.sha256()
-//					.hashString("your input", StandardCharsets.UTF_8)
-//					.toString();
-//
-//		}else{
-//			System.exit(1);
-//		}
-//
-//        try {
-//            AES_CBC_DEMO.main(args);
-//        } catch (Exception e) {
-//            throw new RuntimeException(e);
-//        }
-//        System.exit(0);
+        String response = null;
 
-		String type = null;
-		String mode = null;
-		String text = null;
-		String textInputType = null;
-		String key = null;
-		String iv = null;
-		String putPutFile = null;
+        try {
+            // args:
+            // 0=type(CBC|GCM|ECB) 1=mode(ENCRYPT|DECRYPT) 2=textOrFile
+            // 3=inputType(TEXT|FILE) 4=keyHex  [CBC: 5=ivHex, (FILE:6=out)]
+            // [ECB/GCM: (FILE:5=out)]
+            type = args[0];
+            mode = args[1];
+            text = args[2];
+            textInputType = args[3];
+            key = args[4];
 
-		String response = null;
+            EnumAESType aesType = EnumAESType.valueOf(type.toUpperCase());
+            EnumInputType inputType = EnumInputType.valueOf(textInputType.toUpperCase());
 
-		try {
+            if (aesType == CBC) {
+                iv = args[5];
+                if (inputType == FILE) outPutFile = args[6];
+            } else {
+                iv = "Not required";
+                if (inputType == FILE) outPutFile = args[5];
+            }
 
-			type = args[0];
-			mode = args[1];
-			text = args[2];
-			textInputType = args[3];
-			key = args[4];
+            EnumAESMode aesMode = EnumAESMode.valueOf(mode.toUpperCase());
 
-			EnumAESType aesType = EnumAESType.valueOf(type.toUpperCase());
-			EnumInputType inputType = EnumInputType.valueOf(textInputType.toUpperCase());
+            if (aesType == INVALID_AES_TYPE)  throw new Exception(THROW_INVALID_AES_TYPE);
+            if (aesMode == INVALID_AES_MODE)  throw new Exception(THROW_INVALID_AES_MODE);
+            if (inputType == INVALID_INPUT_TYPE) throw new Exception(THROW_INVALID_INPUT_TYPE);
 
-			if (aesType == CBC){
-				iv = args[5];
+            switch (aesType) {
+                case CBC:
+                    response = CBCFlow(aesType, aesMode, text, inputType, key, iv, outPutFile);
+                    break;
+                case GCM:
+                    response = GCMFlow(aesType, aesMode, text, inputType, key, iv, outPutFile);
+                    break;
+                case ECB:
+                    response = ECBFlow(aesType, aesMode, text, inputType, key, iv, outPutFile);
+                    break;
+                default:
+                    break;
+            }
 
-				if(inputType == FILE)
-					putPutFile = args[6];
+        } catch (GeneralSecurityException gse) {
+            gse.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
-			}else {
-				iv = "Not required";
+        System.out.println(response);
+    }
 
-				if(inputType == FILE)
-					putPutFile = args[5];
-			}
+    public static String CBCFlow(EnumAESType aesType, EnumAESMode aesMode, String text,
+                                 EnumInputType inputType, String key, String iv, String outPutFile) {
+        String response = null;
+        AESFileCipher aesFileCipher = new AESFileCipher();
+        AESCBCCipher cbcCipher = new AESCBCCipher();
 
-			EnumAESMode aesMode = EnumAESMode.valueOf(mode.toUpperCase());
+        try {
+            switch (inputType) {
+                case TEXT:
+                    if (aesMode == ENCRYPT)      response = cbcCipher.encrypt(text, key, iv);
+                    else if (aesMode == DECRYPT) response = cbcCipher.decrypt(text, key, iv);
+                    break;
 
-			if (aesType == INVALID_AES_TYPE)
-				throw new Exception(THROW_INVALID_AES_TYPE);
+                case FILE:
+                    // Construir args para AESFileCipher.{FileEncrypt|FileDecrypt}
+                    String[] fArgs = { aesType.name(), text, key, iv, outPutFile, CBC.name() };
+                    if (aesMode == ENCRYPT) {
+                        aesFileCipher.FileEncrypt(fArgs);
+                        response = SUCCESS;
+                    } else { // DECRYPT habilitado
+                        AESFileCipher.FileDecrypt(fArgs);
+                        response = SUCCESS;
+                    }
+                    break;
 
-			if (aesMode == INVALID_AES_MODE)
-				throw new Exception(THROW_INVALID_AES_MODE);
+                default: break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
 
-			if (inputType == INVALID_INPUT_TYPE)
-				throw new Exception(THROW_INVALID_INPUT_TYPE);
+    public static String GCMFlow(EnumAESType aesType, EnumAESMode aesMode, String text,
+                                 EnumInputType inputType, String key, String iv, String outPutFile) {
+        String response = null;
+        AESFileCipher aesFileCipher = new AESFileCipher();
+        AESGCMCipher gcmCipher = new AESGCMCipher();
 
-			switch (aesType){
-				case CBC:
-					response = CBCFlow(aesType, aesMode, text, inputType, key, iv, putPutFile);
-					break;
-				case GCM:
-					response = GCMFlow(aesType, aesMode, text, inputType, key, iv, putPutFile);
-					break;
-				case ECB:
-					response = ECBFlow(aesType, aesMode, text, inputType, key, iv, putPutFile);
-					break;
-				default:
-					break;
-			}
+        try {
+            switch (inputType) {
+                case TEXT:
+                    if (aesMode == ENCRYPT)      response = gcmCipher.encrypt(text, key);
+                    else if (aesMode == DECRYPT) response = gcmCipher.decrypt(text, key);
+                    break;
 
-		} catch (GeneralSecurityException gse) {
-			gse.printStackTrace();
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-		
-		System.out.println(response);
+                case FILE:
+                    String[] fArgs = { aesType.name(), text, key, iv, outPutFile, GCM.name() };
+                    if (aesMode == ENCRYPT) {
+                        aesFileCipher.FileEncrypt(fArgs);
+                        response = SUCCESS;
+                    } else { // DECRYPT habilitado
+                        AESFileCipher.FileDecrypt(fArgs);
+                        response = SUCCESS;
+                    }
+                    break;
 
-		/*
-		String decryptedValue = null;
-		String paramPathFile = "/Users/xxx/Documents/SSL/as400-gcm-params.properties";
-		String key = "F01F0EE95D8DD60A310BAB4B1584A8E2F105C9BD5A03F4264AF685206D2CD4FA";
-		String iv = "25EC3602CA3CEC05A534B00EB10F4A12";
-		String aesType = "GCM";
-		String tagProps = "as400";
-		String fieldProp = "str.tps.u";
+                default: break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
 
-		EnumAESType enumAESType = EnumAESType.valueOf(aesType.toUpperCase());
+    public static String ECBFlow(EnumAESType aesType, EnumAESMode aesMode, String text,
+                                 EnumInputType inputType, String key, String iv, String outPutFile) {
+        String response = null;
+        AESFileCipher aesFileCipher = new AESFileCipher();
+        AESECBCipher ecbCipher = new AESECBCipher();
 
-		try {
-			decryptedValue = AESUtils.getCipherValue(paramPathFile, tagProps, fieldProp, enumAESType);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-		*/
+        try {
+            switch (inputType) {
+                case TEXT:
+                    if (aesMode == ENCRYPT)      response = ecbCipher.encrypt(text, key);
+                    else if (aesMode == DECRYPT) response = ecbCipher.decrypt(text, key);
+                    break;
 
-	}
+                case FILE:
+                    // En ECB el IV se ignora; mantenemos el contrato de args.
+                    String[] fArgs = { aesType.name(), text, key, iv, outPutFile, ECB.name() };
+                    if (aesMode == ENCRYPT) {
+                        aesFileCipher.FileEncrypt(fArgs);   // FIX: usar ECB, no GCM
+                        response = SUCCESS;
+                    } else { // DECRYPT habilitado
+                        AESFileCipher.FileDecrypt(fArgs);
+                        response = SUCCESS;
+                    }
+                    break;
 
-	public static String CBCFlow(EnumAESType aesType, EnumAESMode aesMode, String text, EnumInputType inputType, String key, String iv, String outPutFile){
-		String response = null;
-		AESFileCipher aesFileCipher = new AESFileCipher();
-		AESCBCCipher cbcCipher = new AESCBCCipher();
-
-		try {
-			switch(inputType){
-				case TEXT:
-					if (aesMode == ENCRYPT){
-						response = cbcCipher.encrypt(text, key, iv);
-					}else if(aesMode == DECRYPT){
-						response = cbcCipher.decrypt(text, key, iv);
-					}
-
-					break;
-				case FILE:
-					if (aesMode == ENCRYPT){
-						String args[] = {aesType.name(), text, key, iv, outPutFile, CBC.name()};
-						aesFileCipher.FileEncrypt(args);
-						response = SUCCESS;
-					}else {
-						response = "AES CBC File Decrypt Mode is not allowed";
-					}
-
-					break;
-				default:
-					break;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return response;
-	}
-
-	public static String GCMFlow(EnumAESType aesType, EnumAESMode aesMode, String text, EnumInputType inputType, String key, String iv, String outPutFile){
-		String response = null;
-		AESFileCipher aesFileCipher = new AESFileCipher();
-		AESGCMCipher gcmCipher = new AESGCMCipher();
-
-		try{
-			switch(inputType) {
-				case TEXT:
-					if(aesMode == ENCRYPT){
-						response = gcmCipher.encrypt(text, key);
-					}else if (aesMode == DECRYPT){
-						response = gcmCipher.decrypt(text, key);
-					}
-
-					break;
-				case FILE:
-					if (aesMode == ENCRYPT){
-						String args[] = {aesType.name(), text, key, iv, outPutFile, GCM.name()};
-						aesFileCipher.FileEncrypt(args);
-						response = SUCCESS;
-					}else {
-						response = "AES GCM File Decrypt Mode is not allowed";
-					}
-
-					break;
-				default:
-					break;
-			}
-
-		} catch (GeneralSecurityException gse) {
-			gse.printStackTrace();
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-
-		return response;
-	}
-
-	public static String ECBFlow(EnumAESType aesType, EnumAESMode aesMode, String text, EnumInputType inputType, String key, String iv, String outPutFile){
-		String response = null;
-		AESFileCipher aesFileCipher = new AESFileCipher();
-		AESECBCipher ecbCipher = new AESECBCipher();
-
-		try{
-			switch(inputType) {
-				case TEXT:
-					if(aesMode == ENCRYPT){
-						response = ecbCipher.encrypt(text, key);
-					}else if (aesMode == DECRYPT){
-						response = ecbCipher.decrypt(text, key);
-					}
-
-					break;
-				case FILE:
-					if (aesMode == ENCRYPT){
-						String args[] = {aesType.name(), text, key, iv, outPutFile, GCM.name()};
-						aesFileCipher.FileEncrypt(args);
-						response = SUCCESS;
-					}else {
-						response = "AES GCM File Decrypt Mode is not allowed";
-					}
-
-					break;
-				default:
-					break;
-			}
-
-		} catch (GeneralSecurityException gse) {
-			gse.printStackTrace();
-		} catch (Exception e){
-			e.printStackTrace();
-		}
-
-		return response;
-	}
+                default: break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return response;
+    }
 }
